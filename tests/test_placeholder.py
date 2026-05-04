24329dc4db8fa1498280994fe94e912c758eee80
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2026-05-04 15:53:17 -0500

    Fix linting and formatting for placeholder test

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -1,5 +1,9 @@
-import pytest
+"""Placeholder test."""
+
 
 class TestPlaceholder:
+    """Placeholder test."""
+
     def test_placeholder(self):
+        """Placeholder test."""
         assert True
